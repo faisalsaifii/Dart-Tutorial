0363879ea096a25245be2339da5cec5e0cc8d60d
faisalsaifii <m.faisal.s@outlook.com>
2022-10-13 22:26:00 +0530

    first commit

---
+++ main.dart
@@ -0,0 +1,23 @@
+void main() {
+  // Single LineComment
+  /* Multi Line Comment */
+
+  print('Hello'); // Print stuff
+  // \n included
+
+  // DataTypes
+  int i = 1;
+  print(i);
+
+  String str = "Faisal";
+  print(str);
+
+  bool isTrue = true;
+  print(isTrue);
+
+  // Dynamic datatype -> Any type of value
+  dynamic dvar = "faisal";
+  print(dvar);
+  dvar = 420;
+  print(dvar);
+}
